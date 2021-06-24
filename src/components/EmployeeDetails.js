b2217c4819f04ecfdc29856439f2c9f4fc8be4c2
Liv <lopez.olivia17@outlook.com>
2021-06-24 11:40:05 -0500

    adjusted file structure

--- src/components/EmployeeDetails.js
+++ src/components/EmployeeDetails.js
@@ -5,9 +5,9 @@ function EmployeeDetails(props) {
   return (
     <tbody>
       {props.results.map(results => (
-        <tr>
+        <tr key={results.email}>
           <td><img alt="Employee" src={results.picture.thumbnail} /></td> 
-          <td>{results.name.first}{results.name.last}</td>
+          <td>{results.name.first} {results.name.last}</td>
           <td>{results.phone}</td>
           <td><a href={"mailto:" + results.email}>{results.email}</a></td>
           <td>{ format(new Date(results.dob.date), 'MM/dd/yyyy')}</td>
